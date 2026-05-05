7639de43a08f0821845aba1aa7714b40e40b8b7e
fran <franmichelle153@gmail.com>
2026-05-06 03:00:21 +0800

    change

--- Rentivator/app/Http/Controllers/ProfileController.php
+++ Rentivator/app/Http/Controllers/ProfileController.php
@@ -27,11 +27,12 @@ class ProfileController extends Controller
         $user->phone_number = $request->phone_number;
         $user->address      = $request->address;
 
-       if ($request->hasFile('profile_picture')) {
-    $file   = $request->file('profile_picture');
-    $mime   = $file->getMimeType();
-    $base64 = base64_encode(file_get_contents($file->getRealPath()));
-    $user->profile_picture = 'data:' . $mime . ';base64,' . $base64;
+      if ($request->hasFile('profile_picture')) {
+    if ($user->profile_picture && !str_starts_with($user->profile_picture, 'data:')) {
+        Storage::disk('public')->delete($user->profile_picture);
+    }
+    $path = $request->file('profile_picture')->store('profile-pictures', 'public');
+    $user->profile_picture = $path;
 }
 
 $user->save();
@@ -40,7 +41,7 @@ return response()->json(['success' => true, 'user' => [
     'name'            => $user->name,
     'phone_number'    => $user->phone_number,
     'address'         => $user->address,
-    'profile_picture' => $user->profile_picture ?? null,
+    'profile_picture' => $user->profile_picture ? Storage::url($user->profile_picture) : null,
 ]]);
     }
 
